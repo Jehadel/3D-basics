bcc77002e4eb518d10a58d36d0c7d894bac788a4
Jehadel <jean.delpech@gmail.com>
2024-12-08 23:51:20 +0100

    Fix: Edges are now only drawn once

--- src/main.lua
+++ src/main.lua
@@ -6,9 +6,9 @@ local a1 = 0
 local a2 = 0 
 local d = 1000
 
-
 local SCREEN_W = 800
 local SCREEN_H = 600
+
 
 -- create a function to return the transformation
 -- matrix to compute the object coordinates
@@ -34,6 +34,7 @@ function viewpointParam(pRho, pTheta, pPhi)
   return vpMat
 
 end
+
 
 -- transform the x,y,z coordinates in the world ref
 -- to the coordinates in the viewpoint ref
@@ -68,35 +69,74 @@ function screenProj(pCoords, pDistance)
 end
 
 
+-- to compare edges we have to sort vertices : 
+-- edge (2, 1) is the same that edge (1, 2)
+function sortEdges(v1, v2)
+
+  local e = {}
+  e.v = {math.min(v1, v2), math.max(v1, v2)} 
+  e.key = tostring(e.v[1])..','..tostring(e.v[2]) -- has in Lua we can’t compare tables, we build a unique key for edges (key = ordered vertices)
+  return e
+
+end
+
+
+function uniqueEdges(faces)
+
+  local uEdges = {}
+
+  for _, face in ipairs(faces) do
+    for i = 1, #face do
+      local v1 = face[i]
+      local v2 = face[i % #face + 1] -- next vertex (when last one is reached, next is the first)  
+      local sortedV = sortEdges(v1, v2)
+      if not uEdges[sortedV.key] then
+        uEdges[sortedV.key] = {sortedV.v[1], sortedV.v[2]}
+      end
+    end
+  end 
+
+  return uEdges
+
+end
+
+
 function drawObject(pRho, pTheta, pPhi, pDistance)
 
+  -- determine which faces are visible (compute normal for each face) 
+
+
+  -- extract from faces "unique" edges that will be drawn (edges can’t appear twice in the list) 
+ 
+  local edgesToDraw = uniqueEdges(obj.faces)
+ 
   local transfMat = viewpointParam(pRho, pTheta, pPhi)
 
-  for i, face in ipairs(obj.faces) do
-  local screenPoints = {}
-  local refPoints = {}
+  -- iterate through unique edges list and 
+  -- compute projection of corresponding vertices on screen
 
-    for j, vertice in ipairs(face) do
-      point = {}
-      point.x = obj.vertices[vertice][1]
-      point.y = obj.vertices[vertice][2]
-      point.z = obj.vertices[vertice][3]
+  for _, edge in pairs(edgesToDraw) do
 
-      transformedPoint = pvTransform(point, transfMat)
-      table.insert(refPoints, transformedPoint)
-      table.insert(screenPoints, screenProj(transformedPoint, pDistance))
-    end
+    p1 = {}
+    p1.x = obj.vertices[edge[1]][1]
+    p1.y = obj.vertices[edge[1]][2]
+    p1.z = obj.vertices[edge[1]][3]
+    transformedPoint1 = pvTransform(p1, transfMat)
+    screenPoint1 = screenProj(transformedPoint1, pDistance)
 
-    table.insert(screenPoints, screenPoints[1])
+    p2 = {}
+    p2.x = obj.vertices[edge[2]][1]
+    p2.y = obj.vertices[edge[2]][2]
+    p2.z = obj.vertices[edge[2]][3]
+    transformedPoint2 = pvTransform(p2, transfMat)
+    screenPoint2 = screenProj(transformedPoint2, pDistance)
 
-    for idx = 1, #screenPoints-1 do
-      love.graphics.line(
-        screenPoints[idx].x + SCREEN_W/2,
-        screenPoints[idx].y + SCREEN_H/2,
-        screenPoints[idx+1].x + SCREEN_W/2,
-        screenPoints[idx+1].y + SCREEN_H/2
-        )
-    end
+    love.graphics.line(
+      screenPoint1.x + SCREEN_W/2,
+      screenPoint1.y + SCREEN_H/2,
+      screenPoint2.x + SCREEN_W/2,
+      screenPoint2.y + SCREEN_H/2
+      )
   end
 
 end
@@ -112,8 +152,8 @@ end
 
 function love.update(dt)
 
-  a1 = a1 + 0.1
-  a2 = a2 + 1
+  a1 = a1 + 5 * dt
+  a2 = a2 + 50 * dt
 
 end
 
